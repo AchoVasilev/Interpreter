6e6e9b3c205451f1844a01f9cf9e3ea16c10f128
Angel Vasilev <angavasilev@gmail.com>
2025-07-28 23:42:59 +0300

    add parser test

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"interpreter.com/m/ast"
+	"interpreter.com/m/lexer"
+	"interpreter.com/m/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so currToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
